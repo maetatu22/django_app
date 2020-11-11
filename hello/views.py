d403088dd1e7b1c6be469fe2fa6fdd10ac1c1734
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-11 21:32:54 +0900

    nextページ作成

--- hello/views.py
+++ hello/views.py
@@ -5,6 +5,15 @@ def index(request):
     params = {
       'title':'Hello/Index',
       'msg':'これは、サンプルで作ったページです。',
+      'goto':'next',
+    }
+    return render(request, 'hello/index.html', params)
+
+def next(request):
+    params = {
+      'title':'Hello/Next',
+      'msg':'これは、もう1つのページです。',
+      'goto':'index',
     }
     return render(request, 'hello/index.html', params)
 
