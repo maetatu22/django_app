ebe44c18de141c278bf59a8a43a40d3aa4728dc9
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-13 01:16:47 +0900

    選択メニューに変更

--- hello/forms.py
+++ hello/forms.py
@@ -4,7 +4,9 @@ class HelloForm(forms.Form):
     data = [
           ('one', 'radio 1'),
           ('two', 'radio 2'),
-          ('three', 'radio 3')
+          ('three', 'radio 3'),
+          ('four', 'item 5'),
+          ('five', 'item 5'),
     ]
     choice = forms.ChoiceField(label='radio', \
-            choices=data, widget=forms.RadioSelect())   
+            choices=data, widget=forms.Select(attrs={'size': 5}))
