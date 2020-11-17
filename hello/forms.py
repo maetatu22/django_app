81c4ceb1bea613d9990301cbc5aa25299affcd8c
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-17 23:34:32 +0900

    指定のIDのレコードだけ取り出す

--- hello/forms.py
+++ hello/forms.py
@@ -1,12 +1,4 @@
 from django import forms
 
 class HelloForm(forms.Form):
-    data = [
-          ('one', 'radio 1'),
-          ('two', 'radio 2'),
-          ('three', 'radio 3'),
-          ('four', 'item 5'),
-          ('five', 'item 5'),
-    ]
-    choice = forms.ChoiceField(label='radio', \
-            choices=data, widget=forms.Select(attrs={'size': 5}))
+    id = forms.IntegerField(label='ID')
