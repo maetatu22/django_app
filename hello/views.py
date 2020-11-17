45e061c6919baae2f4c82176dd7fb53fde20d505
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-17 23:46:13 +0900

    モデルのリストを調べる

--- hello/views.py
+++ hello/views.py
@@ -1,22 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Friend
-from .forms import HelloForm
 
 def index(request):
+    data = Friend.objects.all()
     params = {
-            'title': 'Hello',
-            'message': 'all friends.',
-            'form': HelloForm(),
-            'data': [],
-        }
-    if (request.method == 'POST'):
-        num=request.POST['id']
-        item = Friend.objects.get(id=num)
-        params['data'] = [item]
-        params['form'] = HelloForm(request.POST)
-    else:
-        params['data'] = Friend.objects.all()
+        'title': 'Hello',
+        'data': data,
+    }
     return render(request, 'hello/index.html', params)
 
 
