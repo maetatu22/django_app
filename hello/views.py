e913faeadb3216247b901a71cfeedc01297c6991
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-17 23:53:37 +0900

    valuesメソッド　特定の項目だけを取り出す

--- hello/views.py
+++ hello/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .models import Friend
 
 def index(request):
-    data = Friend.objects.all().values()
+    data = Friend.objects.all().values('id', 'name')
     params = {
         'title': 'Hello',
         'data': data,
