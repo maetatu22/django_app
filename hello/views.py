1753eff26b6d2c844d910f776c2ca1473f19d81e
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-17 22:27:33 +0900

    レコード表示　準備

--- hello/views.py
+++ hello/views.py
@@ -1,25 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
-from .forms import HelloForm
+from .models import Friend
 
-class HelloView(TemplateView):
-
-    def __init__(self):
-        self.params = {
-              'title': 'Hello',
-              'form': HelloForm(),
-              'result': None
+def index(request):
+    data = Friend.objects.all()
+    params = {
+            'title': 'Hello',
+            'message': 'all friends.',
+            'data': data,
         }
-
-    def get(self, request):
-        return render(request, 'hello/index.html', self.params)
-
-    def post(self, request):
-        ch = request.POST['choice']
-        self.params['result'] = 'selected: "' + ch + '".'
-        self.params['form'] = HelloForm(request.POST)
-        return render(request, 'hello/index.html', self.params)
-
+    return render(request, 'hello/index.html', params)
 
 
