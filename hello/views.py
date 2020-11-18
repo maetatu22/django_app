a253d1b85c809dca21268f2dd25b262be3222eee
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-19 00:08:32 +0900

    first, last, countメソッド練習

--- hello/views.py
+++ hello/views.py
@@ -3,11 +3,15 @@ from django.http import HttpResponse
 from .models import Friend
 
 def index(request):
-    data = Friend.objects.all().values_list('id', 'name', 'age')
+    num = Friend.objects.all().count()
+    first = Friend.objects.all().first()
+    last = Friend.objects.all().last()
+    data = [num, first, last]
     params = {
-        'title': 'Hello',
-        'data': data,
-    }
+            'title': 'Hello',
+            'data': data,
+        }
     return render(request, 'hello/index.html', params)
+
 
 
